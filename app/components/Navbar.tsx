7e9016bf340c73e805c193cf02c58111da25fb1f
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-08-30 13:08:47 +0530

    Updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
 
 export function Navbar() {
   return (
     <nav className="w-full border-b">
-      <h1>Hello From The Navbar</h1>
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
+          <Image
+            src={MobileLogo}
+            alt="Mobile Logo"
+            className="block lg:hidden w-12"
+          />
+        </Link>
+        <div className="rounded-full border px-5 py-2">
+        <h1>Hello From the Search</h1>
+        </div>
+      </div>
     </nav>
   );
 }
