8c46a4546383bf6441c23f1c95fa45bc24c1c722
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-11 15:57:03 +0530

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
           port: "",
         },
         {
-          hostname: "glvmmupiqwlmhicmggqp.supabase.co",
+          hostname: "hxwsvfhgsutyckbbvvww.supabase.co",
           protocol: "https",
           port: "",
         },
