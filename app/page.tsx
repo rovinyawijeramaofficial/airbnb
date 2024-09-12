6c0af92d205ea6059b373a0dd43719792266fbf4
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-12 12:52:25 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,73 @@
 import { Suspense, use } from "react";
-import { ListingCard } from "./components/ListingCard";
-import { MapFilterItems } from "./components/MapFilterItems";
-import { SkeltonCard } from "./components/SkeletonCard";
-import prisma from "./lib/db";
 
+import { MapFilterItems } from "./components/MapFilterItems";
+import prisma from "./lib/db";
+import { SkeltonCard } from "./components/SkeletonCard";
+import { NoItems } from "./components/NoItem";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ListingCard } from "./components/ListingCard";
+import { unstable_noStore as noStore } from "next/cache";
 
 async function getData({
-  searchParams, 
+  searchParams,
+  userId,
 }: {
+  userId: string | undefined;
   searchParams?: {
     filter?: string;
+    country?: string;
+    guest?: string;
+    room?: string;
+    bathroom?: string;
   };
 }) {
+  noStore();
   const data = await prisma.home.findMany({
     where: {
       addedCategory: true,
       addedLoaction: true,
       addedDescription: true,
       categoryName: searchParams?.filter ?? undefined,
+      country: searchParams?.country ?? undefined,
+      guests: searchParams?.guest ?? undefined,
+      bedrooms: searchParams?.room ?? undefined,
+      bathrooms: searchParams?.bathroom ?? undefined,
     },
     select: {
       photo: true,
       id: true,
       price: true,
-      description: true, 
-      country: true, 
+      description: true,
+      country: true,
+      Favorite: {
+        where: {
+          userId: userId ?? undefined,
+        },
       },
-  }); 
+    },
+  });
 
   return data;
 }
 
 export default function Home({
-  searchParams, 
+  searchParams,
 }: {
   searchParams?: {
     filter?: string;
+    country?: string;
+    guest?: string;
+    room?: string;
+    bathroom?: string;
   };
 }) {
   return (
     <div className="container mx-auto px-5 lg:px-10">
       <MapFilterItems />
+
       <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
         <ShowItems searchParams={searchParams} />
       </Suspense>
-          
     </div>
   );
 }
@@ -54,11 +77,24 @@ async function ShowItems({
 }: {
   searchParams?: {
     filter?: string;
+    country?: string;
+    guest?: string;
+    room?: string;
+    bathroom?: string;
   };
 }) {
-  const data = await getData({ searchParams: searchParams});
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData({ searchParams: searchParams, userId: user?.id });
+
   return (
-      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-2 mt-8">
+    <>
+      {data.length === 0 ? (
+        <NoItems
+          
+        />
+      ) : (
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
           {data.map((item) => (
             <ListingCard
               key={item.id}
@@ -66,10 +102,13 @@ async function ShowItems({
               imagePath={item.photo as string}
               location={item.country as string}
               price={item.price as number}
-              
+              userId={user?.id}
+             
             />
           ))}
         </div>
+      )}
+    </>
   );
 }
 
