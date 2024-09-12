ddc0358a3018466e98ddfa3649810b07ea2107b1
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-12 11:02:53 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,90 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Suspense, use } from "react";
+import { ListingCard } from "./components/ListingCard";
+import { MapFilterItems } from "./components/MapFilterItems";
+import { SkeltonCard } from "./components/SkeletonCard";
+import prisma from "./lib/db";
 
-export default function Home() {
+
+async function getData({
+  searchParams, 
+}: {
+  searchParams?: {
+    filter?: string;
+  };
+}) {
+  const data = await prisma.home.findMany({
+    where: {
+      addedCategory: true,
+      addedLoaction: true,
+      addedDescription: true,
+      categoryName: searchParams?.filter ?? undefined,
+    },
+    select: {
+      photo: true,
+      id: true,
+      price: true,
+      description: true, 
+      country: true, 
+      },
+  }); 
+
+  return data;
+}
+
+export default function Home({
+  searchParams, 
+}: {
+  searchParams?: {
+    filter?: string;
+  };
+}) {
   return (
-    <div>
-      <Button>Hello From the Index Page</Button>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+      <Suspense key={searchParams?.filter} fallback={<SkeletonLoading />}>
+        <ShowItems searchParams={searchParams} />
+      </Suspense>
+          
+    </div>
+  );
+}
+
+async function ShowItems({
+  searchParams,
+}: {
+  searchParams?: {
+    filter?: string;
+  };
+}) {
+  const data = await getData({ searchParams: searchParams});
+  return (
+      <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-2 mt-8">
+          {data.map((item) => (
+            <ListingCard
+              key={item.id}
+              description={item.description as string}
+              imagePath={item.photo as string}
+              location={item.country as string}
+              price={item.price as number}
+              
+            />
+          ))}
+        </div>
+  );
+}
+
+function SkeletonLoading() {
+  return (
+    <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
+      <SkeltonCard />
     </div>
   );
 }
