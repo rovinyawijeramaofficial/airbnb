abb90256e1587397e157f212a3711aefcda05a09
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-09-13 09:02:01 +0530

    Updated

--- app/components/SubmitButtons.tsx
+++ app/components/SubmitButtons.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Heart, Loader2 } from "lucide-react"; 
+import { Heart, Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 export function CreationSubmit() {
@@ -70,6 +70,24 @@ export function DeleteFromFavoriteButton() {
           type="submit"
         >
           <Heart className="w-4 h-4 text-primary" fill="#E21C49" />
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function ReservationSubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button className="w-full" disabled>
+          <Loader2 className="w-4 h-4 animate-spin mr-2" /> Please wait...
+        </Button>
+      ) : (
+        <Button className="w-full" type="submit">
+          Make a Reservation!
         </Button>
       )}
     </>
